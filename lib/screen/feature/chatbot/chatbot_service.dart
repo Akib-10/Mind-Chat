f0bcf4ec477862f77e9530e4add565f85b742a75
Arafat Akib <arafatakib100@gmail.com>
2026-06-25 04:19:25 +0600

    add api

---
+++ lib/screen/feature/chatbot/chatbot_service.dart
@@ -0,0 +1,11 @@
+import 'package:google_generative_ai/google_generative_ai.dart';
+import 'chat_message.dart';
+
+class ChatbotService {
+  static const String _apiKey = 'AQ.Ab8RN6LMYRz_ZItBVKsy-XKMfAfORoh0xF-ZIpXWhp4Nr7qHYA';
+
+  late final GenerativeModel _model;
+  late ChatSession _chat;
+
+
+}
