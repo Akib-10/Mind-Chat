edc534f9b04f715fa912d87bf60d1e605d8a69c3
Arafat Akib <arafatakib100@gmail.com>
2026-07-03 22:13:33 +0600

    makeing cards

--- lib/widget/home_card.dart
+++ lib/widget/home_card.dart
@@ -22,51 +22,54 @@ class HomeCard extends StatelessWidget {
       shape: const RoundedRectangleBorder(
         borderRadius: BorderRadius.all(Radius.circular(20))
       ),
-      child: InkWell(
-        borderRadius: const BorderRadius.all(Radius.circular(50)),
-        onTap: homeType.onTap,
-        child: homeType.leftAlign ? Row(
-            children:[
-              //Lottie
-              Container(
-                width: mq.width*.35,
-                padding: homeType.padding,
-                child: Lottie.asset('assets/lottie/${homeType.lottie}',),
-              ),
-              const Spacer(),
-              //title
-              Text(
-                  homeType.title,
-                  style: const TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w500,
-                      letterSpacing: 1
-                  )
-              ),
-              const Spacer(flex: 2,),
-            ]
-        ):
-        Row(
-            children:[
-              const Spacer(flex: 2,),
-              //title
-              Text(
-                  homeType.title,
-                  style: const TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w500,
-                      letterSpacing: 1
-                  )
-              ),
-              const Spacer(),
-
-              //Lottie
-              Container(
-                width: mq.width*.35,
-                padding: homeType.padding,
-                child: Lottie.asset('assets/lottie/${homeType.lottie}',),
-              ),
-            ]
+      child: SizedBox(
+          height: mq.height * .20,
+        child: InkWell(
+          borderRadius: const BorderRadius.all(Radius.circular(50)),
+          onTap: homeType.onTap,
+          child: homeType.leftAlign ? Row(
+              children:[
+                //Lottie
+                Container(
+                  width: mq.width*.35,
+                  padding: homeType.padding,
+                  child: Lottie.asset('assets/lottie/${homeType.lottie}',),
+                ),
+                const Spacer(),
+                //title
+                Text(
+                    homeType.title,
+                    style: const TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                        letterSpacing: 1
+                    )
+                ),
+                const Spacer(flex: 2,),
+              ]
+          ):
+          Row(
+              children:[
+                const Spacer(flex: 2,),
+                //title
+                Text(
+                    homeType.title,
+                    style: const TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                        letterSpacing: 1
+                    )
+                ),
+                const Spacer(),
+        
+                //Lottie
+                Container(
+                  width: mq.width*.35,
+                  padding: homeType.padding,
+                  child: Lottie.asset('assets/lottie/${homeType.lottie}',),
+                ),
+              ]
+          ),
         ),
       ),
       //For animation
