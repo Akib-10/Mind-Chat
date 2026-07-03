1242d5da5deafdd284d6a423f9bc28d49be5557f
Arafat Akib <arafatakib100@gmail.com>
2026-07-03 21:08:32 +0600

    new card

---
+++ lib/screen/feature/pdf_feature.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PdfFeature extends StatefulWidget {
+  const PdfFeature({super.key});
+
+  @override
+  State<PdfFeature> createState() => _PdfFeatureState();
+}
+
+class _PdfFeatureState extends State<PdfFeature> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
