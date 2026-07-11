98a06cbb95b01bfdbd10283de0db44cce30b4484
Arafat Akib <arafatakib100@gmail.com>
2026-07-11 16:36:05 +0600

    trans learning

--- lib/screen/feature/translator_feature.dart
+++ lib/screen/feature/translator_feature.dart
@@ -116,7 +116,70 @@ class _TranslatorFeatureState extends State<TranslatorFeature> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Translator'),
+      ),
+      body: ListView(
+        physics: const BouncingScrollPhysics(),
+        padding: EdgeInsets.only(
+          top: mq.height * .02,
+          bottom: mq.width * .1,
+        ),
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // From Language
+              GestureDetector(
+                onTap: () => _pickLanguage(isFrom: true),
+                child: Container(
+                  height: 50,
+                  width: mq.width * .4,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.blue),
+                    borderRadius: const BorderRadius.all(Radius.circular(15)),
+                  ),
+                  child: Text(
+                    _fromLang,
+                    style: const TextStyle(fontWeight: FontWeight.w500),
+                  ),
+                ),
+              ),
 
+              // swap language btn
+              IconButton(
+                onPressed: _swapLanguages,
+                icon: const Icon(
+                  Icons.repeat_rounded,
+                  color: Colors.grey,
+                ),
+              ),
+
+              // To Language
+              GestureDetector(
+                onTap: () => _pickLanguage(isFrom: false),
+                child: Container(
+                  height: 50,
+                  width: mq.width * .4,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.blue),
+                    borderRadius: const BorderRadius.all(Radius.circular(15)),
+                  ),
+                  child: Text(
+                    _toLang,
+                    style: const TextStyle(fontWeight: FontWeight.w500),
+                  ),
+                ),
+              ),
+            ],
+          ),
+
+
+        ],
+      ),
     );
   }
 }
